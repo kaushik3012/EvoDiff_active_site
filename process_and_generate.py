1a9502e6318aba793227f049dc21039b9cd2c105
Kaushik Raj V Nadar <73692082+kaushik3012@users.noreply.github.com>
2024-06-11 14:58:21 -0400

    Update process_and_generate.py

--- process_and_generate.py
+++ process_and_generate.py
@@ -2,31 +2,29 @@ import pandas as pd
 from evodiff.pretrained import OA_DM_38M, OA_DM_640M
 import torch
 from evodiff.conditional_generation import inpaint_multiple_regions
+from collections import defaultdict
+import pickle as pkl
 
-df = pd.read_excel('/content/uniprotkb_active_site_AND_reviewed_true_2024_05_31.xlsx')
-df.dropna(subset=['Active site'], inplace=True)
-df['Active site'] = df['Active site'].apply(lambda x: [int(i[9:]) for i in x.split('; ') if i.startswith('ACT_SITE')])
-df_sub = df.sample(n=100, random_state=42)
-df_sub['Active site'].values.tolist()
-
-
-checkpoint = OA_DM_38M()
-model, collater, tokenizer, scheme = checkpoint
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model.to(device)
+
+def load_activesite_data(path):
+    df = pd.read_excel(path)
+    df.dropna(subset=['Active site'], inplace=True)
+    df['Active site'] = df['Active site'].apply(lambda x: [int(i[9:]) for i in x.split('; ') if i.startswith('ACT_SITE')])
+    return df
 
 def mask_sequences(sequences, start_ids, end_ids):
-    masked_sequences = []
-    for sequence, starts, ends in zip(sequences, start_ids, end_ids):
-        masked_sequence = sequence
-        offset = 0
-        for start, end in zip(starts, ends):
-            start += offset
-            end += offset
-            masked_sequence = masked_sequence[:start] + '#' * (end - start) + masked_sequence[end:]
-            offset += (end - start) - (end - start)
-        masked_sequences.append(masked_sequence)
-    return masked_sequences
+        masked_sequences = []
+        for sequence, starts, ends in zip(sequences, start_ids, end_ids):
+            masked_sequence = sequence
+            offset = 0
+            for start, end in zip(starts, ends):
+                start += offset
+                end += offset
+                masked_sequence = masked_sequence[:start] + '#' * (end - start) + masked_sequence[end:]
+                offset += (end - start) - (end - start)
+            masked_sequences.append(masked_sequence)
+        return masked_sequences
 
 def tokenize_sequences(sequences, tokenizer, device=device):
     tokenized_sequences = [torch.tensor(tokenizer.tokenizeMSA(seq)) for seq in sequences]
@@ -38,20 +36,48 @@ def prepare_indices(start_ids, end_ids, device=device):
     end_idxs = torch.tensor(end_ids).to(device)
     return start_idxs, end_idxs
 
-idxs = [[(x-1) for x in row] for row in df_sub['Active site'].values.tolist()]
+def generate_sequences(df, model='OADM_38M'):
+    checkpoint = OA_DM_38M()
+    model, collater, tokenizer, scheme = checkpoint
+    model.to(device)
 
-sequences = df_sub['Sequence'].values.tolist()
-sequences = [ele for ele in sequences for i in range(5)]
-start_ids = [ele for ele in idxs for i in range(5)]
-end_ids = df_sub['Active site'].values.tolist()
-end_ids = [ele for ele in end_ids for i in range(5)]
+    activesite_idxs = [[(x-1) for x in row] for row in df['Active site'].values.tolist()]
+    sequences = df['Sequence'].values.tolist()
+    start_ids = activesite_idxs
+    end_ids = df['Active site'].values.tolist()
 
-# Mask the sequences
-masked_sequences = mask_sequences(sequences, start_ids, end_ids)
+    # Mask the sequences
+    masked_sequences = mask_sequences(sequences, start_ids, end_ids)
 
-# Tokenize the masked sequences
-tokenized_sequences = tokenize_sequences(masked_sequences, tokenizer, device)
+    # Tokenize the masked sequences
+    tokenized_sequences = tokenize_sequences(masked_sequences, tokenizer, device)
 
-untokenized_seqs, sequences, untokenized_idrs, sequences_idrs, save_starts, save_ends = inpaint_multiple_regions(model, tokenized_sequences, start_ids, end_ids, sequences, tokenizer)
+    sequences = [ele for ele in sequences for i in range(5)]
+    tokenized_sequences = [ele for ele in tokenized_sequences for i in range(5)]
+    start_ids = [ele for ele in start_ids for i in range(5)]
+    end_ids = [ele for ele in end_ids for i in range(5)]
 
-df_sub['Sequence'].values.tolist()[0]
+    untokenized_seqs, sequences, untokenized_idrs, sequences_idrs, save_starts, save_ends = inpaint_multiple_regions(model, tokenized_sequences, start_ids, end_ids, sequences, tokenizer)
+
+    generated_map = defaultdict((lambda : []))
+    
+    for i, sequence in enumerate(sequences):
+        sequence = sequence[0]
+        generated_map[sequence].append(untokenized_seqs[i][0])
+
+    return generated_map
+    
+def main():
+    activesite_path = '/content/uniprotkb_active_site_AND_reviewed_true_2024_05_31.xlsx'
+    df = load_activesite_data(activesite_path)
+
+    df_sub = df.sample(n=100, random_state=42)
+    generated_map = generate_sequences(df_sub)
+    
+    with open('generated_sequences.pkl', 'wb') as f:
+        pkl.dump(dict(generated_map), f)
+
+    print('Saved the Generated Sequences as generated_sequences.pkl!')
+
+if __name__== "__main__":
+    main()
